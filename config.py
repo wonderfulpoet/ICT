d217dbc8bc4cdd4f41fbe5ebdcc9b4c6b6a16fe9
wonderfulpoet <23050316@hdu.edu.cn>
2025-12-03 00:01:28 +0800

    upload

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 UPLOAD_FOLDER = 'uploads'
-MAX_CONTENT_LENGTH = 16 * 1024 * 1024  # 限制上传文件大小为16MB
+MAX_CONTENT_LENGTH = 1000 * 1024 * 1024  # 限制上传文件大小为16MB
 PORT = 3000
 PASSWORD_FILE = 'password/pswd.csv'
